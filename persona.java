59a3c13ba9dfb59715c5ff5c34d859b384ae392f
Jorge2417 <George2_6aAZ@hotmail.com>
2019-05-06 14:48:55 -0500

    metodoApellidoSALVA

--- persona.java
+++ persona.java
@@ -1,8 +1,13 @@
 public class Persona{
   private nombre;
-  
+ 
   public Persona(){
     this.persona="Desconocido.";
+  }
+
+  public Apellidos(String apellidoNuevo){
+    this.apellidos = this.apellidoNuevo;
+
   }
   
 }
