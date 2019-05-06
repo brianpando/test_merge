bc5704b965f2486900902cfea8af74e734cda3a2
Jorge2417 <George2_6aAZ@hotmail.com>
2019-05-06 14:57:36 -0500

    xdSalva

--- persona.java
+++ persona.java
@@ -1,12 +1,13 @@
 public class Persona{
   private nombre;
- 
+  public String Apellidos;
+
   public Persona(){
     this.persona="Desconocido.";
   }
 
   public Apellidos(String apellidoNuevo){
-    this.apellidos = this.apellidoNuevo;
+    this.Apellidos = this.apellidoNuevo;
 
   }
   
