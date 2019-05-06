02da6283ecb0e8c4fd8307d1b68465fe8f1066e6
Paola <gborja311@gmail.com>
2019-05-06 14:46:41 -0500

    paola_prueba

--- persona.java
+++ persona.java
@@ -1,8 +1,19 @@
 public class Persona{
-  private nombre;
-  
+  private String nombre;
+  private String apellidos;
+  private String dni;
+  private String ciudad;
+
+
   public Persona(){
     this.persona="Desconocido.";
   }
   
+  public Persona(String nombre, String apellidos, String dni, String ciudad){
+    this.nombre = nombre;
+    this.apellidos = apellidos;
+    this.dni = dni;
+    this.ciudad;
+
+  }
 }
