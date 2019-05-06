e51c0d07cba9543a5f23f604ae7bb7ed3b26d21c
salomonSanchez <s.sanchez@unas.edu.pe>
2019-05-06 15:11:38 -0500

    aceptando cambios

--- persona.java
+++ persona.java
@@ -53,13 +53,6 @@ public class Persona{
 
 }
 
-
-
-//imprimir clase completo
-public String toString() {
-    return "nombre: "+this.nombre;
-}
-
 //Agregar persona vecinas
 public void personaVecina(String ves){
   if(this.vecinas == ves){
