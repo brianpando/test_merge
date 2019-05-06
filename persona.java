ed1952b8aa2b346b66e26437fa5e0b89ac43d8a4
Diego <diego.vega097@gmail.com>
2019-05-06 13:54:13 -0500

    Metodo Determinar sexo

--- persona.java
+++ persona.java
@@ -1,8 +1,27 @@
+import javax.swing.JOptionPane;
+
 public class Persona{
-  private nombre;
+  private String nombre;
+  private String sexo;
   
   public Persona(){
     this.persona="Desconocido.";
   }
   
+  public void determinarSexo(){
+
+    JOptionPane.showMessageDialog(null, "Determinar el sexo de una persona");
+    sexo = JOptionPane.showInputDialog(null, "Tiene pito ?..");
+    if (sexo == "si"){
+      JOptionPane.showMessageDialog(null, "ES HOMBREEEEE...!!");
+
+    }else if(sexo == "no"){
+      JOptionPane.showMessageDialog(null, "ES MUJER...!!");
+
+    }else if (sexo != "si" && sexo != "no"){
+      JOptionPane.showMessageDialog(null, "ERES RARITO!!");
+    }
+  }
 }
+
+
